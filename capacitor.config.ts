5742a303971858b382c52242ccea83974fc21556
heliojsalles-art <heliojsalles@gmail.com>
2026-03-09 11:59:42 -0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.heliosalles.organizza',
+  appName: 'Organizza',
+  webDir: 'dist',
+  server: {
+    iosScheme: 'https',
+  },
+};
+
+export default config;
